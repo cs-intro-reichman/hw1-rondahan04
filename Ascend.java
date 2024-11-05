93448a8af9c65a75e38935b625e5d9f87b3159d6
Ron Dahan <rondahan@Rons-MacBook-Pro.local>
2024-11-05 14:21:10 +0200

    Finished Asceding numbers

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,15 @@
-// Generates three integer random numbers in a given range,
+
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		String sLim = args[0];
+		int lim = Integer.parseInt(sLim);
+		int a =(int)(Math.random() * (lim) + 1); // the one is added because if i wont add him the limit number wont appear ever.
+		int b =(int)(Math.random() * (lim) + 1); // and the int before the Mathrandom function is because Mathrandom returns a double and I want and Integer.
+		int c =(int)(Math.random() * (lim) + 1); 
+		int min = Math.min(Math.min(a , b),c);
+		int max = Math.max(Math.max(a,b),c);
+		int mid = a+b+c-min-max;
+		System.out.println (min + " "+ mid + " " + max);
 	}
 }
