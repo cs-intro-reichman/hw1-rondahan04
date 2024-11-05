65ba2de67198a39bcbac5c70e301f583154d11f4
Ron Dahan <rondahan@Rons-MacBook-Pro.local>
2024-11-05 12:43:44 +0200

    Completed the Bill3 exercise

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,13 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
+	    String name1 = (args[0]);
+		String name2 = (args[1]);
+		String name3 = (args[2]);
+		String bills = (args[3]);
+		int bill = Integer.parseInt(bills);
+		bill = bill/3;
+		System.out.println ("Dear " + name1 + ", " + name2 + ", and " +name3 + ": " + "pay " + bill +" each.");
 	    // Replace this comment with the rest of your code   
 	}
 }
