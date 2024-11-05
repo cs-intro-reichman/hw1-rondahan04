f04a77cbb6baa36981a39456a979da27411f15b7
rondahan04 <ron.dahan01@post.runi.ac.il>
2024-11-05 16:33:21 +0200

    Update Ascend.java

--- Ascend.java
+++ Ascend.java
@@ -4,7 +4,7 @@ public class Ascend {
 	public static void main(String[] args) {
 		int lim = args[0];
 		int a =(int)(Math.random() * (lim) + 1); // the one is added because if i wont add him the limit number wont appear ever.
-		int b =(int)(Math.random() * (lim) + 1); // and the int before the Mathrandom function is because Mathrandom returns a double and I want and Integer.
+		int b =(int)(Math.random() * (lim) + 1); // and the int before the Mathrandom function is because Mathrandom returns a double and I want an Integer.
 		int c =(int)(Math.random() * (lim) + 1); 
 		int min = Math.min(Math.min(a , b),c);
 		int max = Math.max(Math.max(a,b),c);
